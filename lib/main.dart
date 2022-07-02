c7ec45a8bb0ac9dfed6975b76b79fc51c934dc20
ung <test@abc.com>
2022-07-02 15:58:03 +0700

    finish 2July

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:unggoodstudent/states/authen.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverride();
   runApp(MyApp());
 }
 
@@ -13,5 +16,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Authen(),
     );
+  }
+}
+
+class MyHttpOverride extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    // TODO: implement createHttpClient
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (cert, host, port) => true;
   }
 }
